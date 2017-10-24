ccae180c6f0badaaf802c1e9d6932f8ffd3e18dd
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2017-10-24 19:32:55 +0000

    Finished cart functionality, can now update quantity and delete cart items.

--- cart.php
+++ cart.php
@@ -6,8 +6,37 @@
 
 <?php include'navigation.php'?>
 
+<?php
+if(isset($_POST['refresh'])){ 
+
+		foreach($_POST['quantity'] as $key => $val) { 
+
+    		if($val == 0) { 
+        		unset($_SESSION['cart'][$key]); 
+		}
+		else{ 
+        		$_SESSION['cart'][$key]['quantity']=$val; 
+    		}	 
+	} 
+  
+}
+?>
+
+<?php 
+
+if (isset($_GET['action']) && $_GET['action'] == "delete") {
+	echo "test";
+	$id = intval ($_GET['id']);
+
+	if (isset($_SESSION['cart'][$id])) {
+		unset($_SESSION['cart'][$id]);
+	}
+}
+
+?>
 
 <div class="container">
+<form method="post" action="cart.php">
   <table id="cart" class="table table-hover table-condensed">
             <thead>
             <tr>
@@ -25,8 +54,6 @@
 
 	<?php $sql = "select * from products where id in (";?>
 
-	<?php echo "TEST"; ?>
-
 	<?php foreach ($_SESSION['cart'] as $id => $value): ?>
 
 		<?php $sql.=$id.",";?>		
@@ -36,38 +63,36 @@
 	<?php $sql = substr($sql, 0, -1).") order by name asc"; ?>
 
 	<?php $query = mysqli_query ($conn, $sql); ?>
-
+	<?php $totalPrice = 0; ?>
 	<?php while ($row = mysqli_fetch_array($query)): ?>
-
-		<?php $cartArr[] = $row; ?>
-
-	<?php endwhile; ?>
-
-	<?php foreach ($cartArr as $output): ?>
-
+		<?php $subtotal = $_SESSION['cart'][$row['id']]['quantity'] * $row['price']; ?>
+		<?php $totalPrice += $subtotal; ?>
+		<?php $quantity = $_SESSION['cart'][$row['id']]['quantity']; ?>
+	
             <tr>
               <td data-th="Product">
                 <div class="row">
-		<div class="col-sm-2 hidden-xs"><img src="<?php echo $output['image'] ?>" alt="..." class="img-responsive"/></div>
+		<div class="col-sm-2 hidden-xs"><img src="<?php echo $row['image'] ?>" alt="..." class="img-responsive"/></div>
                   <div class="col-sm-10">
-		  <h4 class="nomargin"><?php echo $output['name'] ?></h4>
+		  <h4 class="nomargin"><?php echo $row['name'] ?></h4>
 			
                     <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
                   </div>
                 </div>
               </td>
-              <td data-th="Price">$7.95</td>
+	      <td data-th="Price">$<?php echo $row['price']?></td>
               <td data-th="Quantity">
-                <input type="number" class="form-control text-center" value="1">
+	      <input type="number" name="quantity[<?php echo $row['id'] ?>]" class="form-control text-center" value="<?php echo $_SESSION['cart'][$row['id']]['quantity'] ?>">
               </td>
               <td data-th="Subtotal" class="text-center">$7.95</td>
               <td class="actions" data-th="">
-                <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-                <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>                
+	      <button type="submit" name="refresh" class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+	      <a href="cart.php?action=delete&id=<?php echo $row['id'] ?>" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></a>
               </td>
             </tr>
 
-	<?php endforeach; ?>
+
+	<?php endwhile; ?>
 
 <?php else: echo "Your cart is empty.";?>
 
@@ -83,12 +108,12 @@
             <tr>
               <td><a href="store.php" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
               <td colspan="2" class="hidden-xs"></td>
-              <td class="hidden-xs text-center"><strong>Total $7.95</strong></td>
+	      <td class="hidden-xs text-center"><strong>$<?php echo money_format('%i', $totalPrice) ?></strong></td>
               <td><a href="checkout.php" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
             </tr>
           </tfoot>
         </table>
 </div>
-
+</form>
 <?php include'footer.php'?>
 
