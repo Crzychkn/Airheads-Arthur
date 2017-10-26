037922cc49436896bff532898f04a8eb18436804
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2017-10-26 15:16:36 +0000

    Changed redirect url from hardcoded link to relative path.

--- thanks.php
+++ thanks.php
@@ -4,7 +4,7 @@
 
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 <! -- Redirect back to home page after 5 seconds -->
-<meta http-equiv="refresh" content="5; url=http://airheads.life" />
+<meta http-equiv="refresh" content="5; url=index.php" />
 
 <?php include'navigation.php'?>
 
