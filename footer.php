e8a9f479fc06e3d81a754453a864e88151853896
Jada Scott <jadalscott@hotmail.com>
2017-10-29 09:40:47 -0600

    Added login modal

--- footer.php
+++ footer.php
@@ -5,8 +5,54 @@
   </a><br><br>
   
 
-  <p>Theme Made By <a href="http://www.uvu.edu" data-toggle="tooltip" title="Visit Using">Airheads Dev Team</a></p>
-  <h6><a href="login.php">ADMIN</a></h6>
+ 
+  <button type="button" class="btn btn-success" data-toggle="modal" data-target="#loginModal"><a class="group inner list-group-item-heading">LOGIN</a></button>
+
+                <!-- Modal -->
+<div id="loginModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        
+        <h4 class="modal-title">ADMIN LOGIN</h4>
+         
+      </div>
+      <div class="modal-body">
+       <div class="formLogin" id="formLogin">
+                        <div class="row">
+                            <div class="container-fluid">
+                                <form>
+                                    <div class="form-group">
+                                        <label class="">Username</label>
+                                        <input class="form-control" name="username" id="username" type="text">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="">Password</label>
+                                        <input class="form-control" name="password" id="password" type="password">
+                                        <br class="">
+                                    </div>
+              
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+               
+      <div class="modal-footer">
+        <a class="btn btn-default" href="admin.php">LOGIN</a>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+
+
 </footer>
 
 <script>
@@ -16,16 +62,12 @@ $(document).ready(function(){
   
   // Add smooth scrolling to all links in navbar + footer link
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-
       // Prevent default anchor click behavior
       event.preventDefault();
-
       // Store hash
       var hash = this.hash;
-
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
@@ -38,12 +80,13 @@ $(document).ready(function(){
     } // End if
   });
 })
-
 $(document).ready(function() {
     $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
     $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
 });
 </script>
+
+<script type="text/javascript" src="js/main.js"></script>
 
 </body>
 </html>
