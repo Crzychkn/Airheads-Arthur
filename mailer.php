622cebe48c237ef83b50cc95928c6f2625e6fa3f
Jada Scott <jadalscott@hotmail.com>
2017-10-30 11:30:28 -0600

    Sends confirmation email to customer and store owner

---
+++ mailer.php
@@ -0,0 +1,50 @@
+<?php
+        if(isset($_POST)){
+        // Get the form fields and remove whitespace.
+        $customerfirst = strip_tags(trim($_POST["customerfirst"]));
+				$customerfirst = str_replace(array("\r","\n"),array(" "," "),$customerfirst);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $totalprice = $_SESSION['totalprice'];
+        
+
+        // Check that data was sent to the mailer.
+        if ( empty($customerfirst) OR empty($customerlast) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            exit;
+        }
+
+        // Set the recipient email address.
+        // FIXME: Update this to your desired email address.
+        $recipient = "jadalscott@gmail.com, $email";
+
+        // Set the email subject.
+        $subject = "Thank you for your order! :)";
+
+        // Build the email content.
+        $email_content = "Name: $customerfirst\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Total: \n$totalprice\n";
+
+        // Build the email headers.
+        $email_headers = "From: $customerfirst <$email>";
+
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "You will receive an order confirmation shortly.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your order.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+
+?>
